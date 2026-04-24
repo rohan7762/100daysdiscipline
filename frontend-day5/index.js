b06e5b216e13818561b196b0ec0e7bf51c64f8f5
Rohan <rohanshree66@gmail.com>
2026-04-24 22:57:38 +0545

    upgraded css and added enter keypress event

--- frontend-day5/index.js
+++ frontend-day5/index.js
@@ -3,22 +3,31 @@ let button = document.getElementById('addBtn');
 let list = document.getElementById('taskList');
 
 
-button.addEventListener("click", function(){
-let task = taskInput.value;
-if(task === ""){
-    return;
-}
-let li = document.createElement("li");
-li.innerText = task;
+button.addEventListener("click", function () {
 
- // 🔥 NEW PART (IMPORTANT)
+    let task = taskInput.value;
+
+    if (task === "") {
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerText = task;
+
     li.addEventListener("click", function () {
         li.remove();
     });
 
+    list.appendChild(li);
 
-list.appendChild(li);
+    taskInput.value = ""; // fixed
+});
+taskInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        button.click();
+    }
+});
+    
 
-taskInput.value = " ";
 
-})
+
